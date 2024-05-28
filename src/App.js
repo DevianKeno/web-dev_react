1b8ec521af8026af929cd4a98d94fe2b845450e1
deviank <tensaidevi@gmail.com>
2024-05-29 04:26:20 +0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
+const images = document.querySelectorAll(".acc-img");
+
+images.forEach((img) => {
+    img.addEventListener("click", () => {
+        const card = img.nextElementSibling;
+        const isExpanded = card.classList.contains("expand");
+        const allCards = document.querySelectorAll(".card");
+
+        allCards.forEach((element) => {
+            if (element !== card) {
+                element.classList.remove("expand");
+                element.classList.add('small');
+            }
+        });
+        if (isExpanded) {
+            card.classList.remove("expand");
+            card.classList.add("small");
+        } else {
+            card.classList.remove("small");
+            card.classList.add('expand');
+        }
+    });
+});
+
 function App() {
   return (
     <div className="App">
@@ -16,7 +40,7 @@ function App() {
           rel="noopener noreferrer"
         >
           Learn React
-        </a>
+        </a>        
       </header>
     </div>
   );
